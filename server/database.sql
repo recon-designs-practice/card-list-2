997558fc0a8dfeea0773743b35669ee0ec7f747a
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2022-08-20 17:21:53 -0500

    added post route to add new card

--- server/database.sql
+++ server/database.sql
@@ -2,20 +2,20 @@ CREATE DATABASE cardlist;
 
 CREATE DATABASE cardbrandlist;
 
-CREATE TABLE card(
+CREATE TABLE cardtable(
     card_id SERIAL PRIMARY KEY,
-    card_number integer,
     card_year integer,
-    card_brand VARCHAR(100),
-    card_set VARCHAR(200),
+    card_brand text,
+    card_set text,
+    card_number integer,
     card_sn_number integer,
     card_sn_total integer,
-    card_attributes VARCHAR(30),
-    player_first_name VARCHAR(100),
-    player_second_name VARCHAR(100),
-    player_team_city VARCHAR(30),
-    player_team_name VARCHAR(30),
-    player_team VARCHAR(3)
+    card_attributes text[],
+    player_first_name text,
+    player_second_name text,
+    team_city text,
+    team_name text,
+    team_name_short text
 );
 
 CREATE TABLE cardbrand(
--- server/index.js
+++ server/index.js
@@ -1,9 +1,98 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const pool = require('./db')
 
 app.use(cors())
 app.use(express.json())
+
+// create a card
+app.post('/cards', async (req, res) => {
+    try {
+        const {
+            card_year,
+            card_brand,
+            card_set,
+            card_number,
+            card_sn_number,
+            card_sn_total,
+            card_attributes,
+            player_first_name,
+            player_second_name,
+            team_city,
+            team_name,
+            team_name_short
+        } = req.body
+
+        const newCard = await pool.query(
+            `INSERT INTO cardtable(
+                card_year,
+                card_brand, 
+                card_set,
+                card_number,
+                card_sn_number,
+                card_sn_total,
+                card_attributes,
+                player_first_name,
+                player_second_name,
+                team_city,
+                team_name,
+                team_name_short
+            ) VALUES (
+                $1,
+                $2,
+                $3,
+                $4,
+                $5,
+                $6,
+                $7,
+                $8,
+                $9,
+                $10,
+                $11,
+                $12
+            ) RETURNING *
+            `,
+            [
+                card_year,
+                card_brand,
+                card_set,
+                card_number,
+                card_sn_number,
+                card_sn_total,
+                card_attributes,
+                player_first_name,
+                player_second_name,
+                team_city,
+                team_name,
+                team_name_short
+            ]
+        )
+
+        res.json(newCard.rows[0])
+
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+// create a brand
+
+// get all cards
+
+// get all brands
+
+// get a card
+
+// get a brand
+
+// edit a card
+
+// edit a brand
+
+// delete a card
+
+// delete a brand
 
 app.listen(5001, () => {
     console.log('Server is listening on port 5001.')
