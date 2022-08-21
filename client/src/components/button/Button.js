3324071d9729907854a0a7cc3d67ab607fc82651
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2022-08-21 17:23:13 -0500

    finished button

--- client/src/components/button/Button.js
+++ client/src/components/button/Button.js
@@ -4,33 +4,70 @@ import PropTypes from 'prop-types'
 
 const StyledButton = styled.button`
     padding: 10px 12px;
-    background: #0E8AD0;
+    background: ${props => props.background};
     border: 2px solid #2D2E31;
     box-shadow: 6px 6px 0px rgba(45, 46, 49, 0.14);
     border-radius: 2px;
 
     &:hover {
-        background: #0C76B2;
+        background: ${props => props.hoverbackground};
         cursor: pointer;
     }
 
     &:active {
-        background: #0B6BA1;
+        background: ${props => props.activeBackground};
         box-shadow: none;
     }
 
     & > p {
-        color: #F2F1EF;
+        color: ${props => props.color};
     }
 `
 
-export default function Button({ children, onclick }) {
-  return (
-    <StyledButton onClick={onclick}>{children}</StyledButton>
+export default function Button({ buttonType, children, onclick }) {
+    const {
+        background,
+        hoverbackground,
+        activeBackground,
+        color
+    } = getButtonStyles(buttonType)
+
+    function getButtonStyles(type) {
+        switch (type) {
+            case 'primary':
+            default:
+                return {
+                    background: '#0E8AD0',
+                    hoverbackground: '#0C76B2',
+                    activeBackground: '#0B6BA1',
+                    color: '#F2F1EF'
+                }
+            case 'secondary':
+                return {
+                    background: 'white',
+                    hoverbackground: '#E0DFDE',
+                    activeBackground: '#CCCBC9',
+                    color: '#2D2E31'
+                }
+        }
+    }
+
+    return (
+    <StyledButton 
+        onClick={onclick}
+        background={background}
+        hoverbackground={hoverbackground}
+        activeBackground={activeBackground}
+        color={color}
+    >{children}</StyledButton>
   )
 }
 
 Button.propTypes = {
+    /**
+     * Selects whether button will be displayed with primary, or secondary styling
+     */
+    buttonType: PropTypes.string,
     /**
      * Children to be passed to the button
      */
--- client/src/stories/Button.stories.js
+++ client/src/stories/Button.stories.js
@@ -3,13 +3,21 @@ import { Paragraph1 } from '../theme/type'
 
 export default {
     title: 'Button',
-    component: Button
+    component: Button,
 }
 
 const Template = (args) => <Button {...args} />
 
 export const PrimaryButton = Template.bind({})
 PrimaryButton.args = {
+    buttonType: 'primary',
     children: <Paragraph1>Primary</Paragraph1>,
     onclick: () => alert('Primary button clicked')
+}
+
+export const SecondaryButton = Template.bind({})
+SecondaryButton.args = {
+    buttonType: 'secondary',
+    children: <Paragraph1>Secondary</Paragraph1>,
+    onclick: () => alert('Secondary button clicked')
 }
