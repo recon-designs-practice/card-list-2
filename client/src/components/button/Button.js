191004fd68e4c4cc0ac4848ce51de6cca2cbb011
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2022-08-21 16:55:31 -0500

    added default styling to Button

--- client/src/components/button/Button.js
+++ client/src/components/button/Button.js
@@ -2,7 +2,25 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledButton = styled.button`
-    background: pink;
+    padding: 10px 12px;
+    background: #0E8AD0;
+    border: 2px solid #2D2E31;
+    box-shadow: 6px 6px 0px rgba(45, 46, 49, 0.14);
+    border-radius: 2px;
+
+    &:hover {
+        background: #0C76B2;
+        cursor: pointer;
+    }
+
+    &:active {
+        background: #0B6BA1;
+        box-shadow: none;
+    }
+
+    & > p {
+        color: #F2F1EF;
+    }
 `
 
 export default function Button({ children }) {
